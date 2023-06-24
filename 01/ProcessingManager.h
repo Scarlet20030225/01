7817a905766e684694729b7488a97f52ec542b9b
Scarlet20030225 <21010022hs@edu.fca.ac.jp>
2023-06-24 15:30:28 +0900

    なんか違う、だからリセット

--- 01/ProcessingManager.h
+++ 01/ProcessingManager.h
@@ -1,5 +1,34 @@
 #pragma once
+
+/// <summary>
+/// 処理のマネージャー
+/// </summary>
 class ProcessingManager
 {
-};
+public:
+	ProcessingManager();
+	~ProcessingManager();
 
+	/// <summary>
+	/// 全ての初期化処理
+	/// </summary>
+	bool AllInitialize();
+
+	/// <summary>
+	/// 全ての終了処理
+	/// </summary>
+	void AllFinalize();
+
+	/// <summary>
+	/// 全ての更新処理
+	/// </summary>
+	void AllUpdate();
+
+	/// <summary>
+	/// 全ての描画処理
+	/// </summary>
+	void AllDraw();
+
+private:
+
+};
--- 01/Window.h
+++ 01/Window.h
@@ -1,5 +1,16 @@
 #pragma once
 
+#include<Windows.h>
+
 class Window
 {
+public:
+	Window();
+	~Window();
+
+	bool Initialize();
+	void Finalize();
+
+private:
+
 };
